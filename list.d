e66c13a5ccca3cc9981e4b57f5e901052e8db5f5
Oran Goshen <ogoshen@BCRICWH.LAN@ganttlab-hpc01.bcricwh.lan>
2017-03-07 15:42:26 -0800

    first running script

---
+++ list.d
@@ -0,0 +1,54 @@
+BASE=/mnt/data/datafiles
+RUN = /home/BCRICWH.LAN/ogoshen/software
+REF = concensus
+
+;needs to be less redundent!
+
+SAMPLE_NAME=505-M
+%call read2inc.d
+
+SAMPLE_NAME=505-Pa
+%call read2inc.d
+
+SAMPLE_NAME=519-Pb
+%call read2inc.d
+
+SAMPLE_NAME=519-Pc
+%call read2inc.d
+
+SAMPLE_NAME=519-Pd
+%call read2inc.d
+
+SAMPLE_NAME=519-S1a
+%call read2inc.d
+
+SAMPLE_NAME=520-Pa
+%call read2inc.d
+
+SAMPLE_NAME=520-Pb
+%call read2inc.d
+
+SAMPLE_NAME=520-Pc
+%call read2inc.d
+
+SAMPLE_NAME=520-S1
+%call read2inc.d
+
+SAMPLE_NAME=520-S1a
+%call read2inc.d
+
+SAMPLE_NAME=579-M
+%call read2inc.d
+
+SAMPLE_NAME=579-Pa
+%call read2inc.d
+
+SAMPLE_NAME=579-Pb
+%call read2inc.d
+
+SAMPLE_NAME=579-S1a
+%call read2inc.d
+
+SAMPLE_NAME=579-S1b
+%call read2inc.d
+
